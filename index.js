53f40c1e79b8fea89fefbae180972f964559a4e4
sota <oceaneye3953@protonmail.com>
2024-10-19 16:08:26 +0900

    release ver1.1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const myOriginalLibrary = (a, b) => {
-    return console.log(a + b);
+    return console.log("a * b = " + (a * b));
 };
 
+myOriginalLibrary(10, 2);
 
 module.exports = myOriginalLibrary;
